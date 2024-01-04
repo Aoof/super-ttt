35f3a7c5e15cc659dc6663328b03fab74a6171e8
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2024-01-03 19:58:02 -0500

    Init

---
+++ pages/game.tsx
@@ -0,0 +1,16 @@
+export default function Game() {
+    return (
+        <main className="h-full flex items-center content-center justify-around">
+            <section>
+                <h1 className="text-center text-6xl my-12 relative">
+                    <div className="absolute top-12 h-12 w-full bg-blue-400 blur-3xl bg-opacity-70"></div>
+                    Super Tic Tac Toe
+                </h1>
+                <p className="text-center text-blue-500">
+                    <a href="/game" className="bg-blue-500 hover:bg-blue-700 text-white py-4 px-10 m-4 rounded transition duration-50 ease-in-out text-xl">Play Online</a>
+                    <a href="/offline" className="bg-blue-500 hover:bg-blue-700 text-white py-4 px-10 m-4 rounded transition duration-50 ease-in-out text-xl">Play VS Computer</a>
+                </p>
+            </section>
+        </main>
+    )
+}
